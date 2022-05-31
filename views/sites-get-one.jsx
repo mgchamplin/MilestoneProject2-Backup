4e85c1d12f298521c5fac27bd2251c3c5be883bc
Michael Champlin <mchamplin@sbcglobal.net>
2022-05-30 19:43:21 -0700

    Center Flex the site list

--- views/sites-get-one.jsx
+++ views/sites-get-one.jsx
@@ -6,8 +6,8 @@ function get_one (data) {
     console.log("get-one.jsx for " + data.site.name)
     return (
       <Def>
+        <br></br>
         <Card className="one-card-show">
-            
             <Card.Title>{data.site.name}</Card.Title>
 
             <Card.Img src={'.'+data.site.image}/>
@@ -18,9 +18,9 @@ function get_one (data) {
             </Card.Text>
             
             <div className="button-group">
-              <a href={`/site/${data.site._id}/edit`} className="edit-button btn btn-primary btn-space">Edit</a>
+              <a href={`/site/${data.site._id}/edit`} className="edit-button btn btn-primary">Edit</a>
               <form method="POST" action={`/site/${data.site._id}?_method=DELETE`}> 
-                  <button type="submit" className="delete-button bi bi-trash btn btn-space">Delete</button>
+                  <button type="submit" className="delete-button bi btn btn-primary">Delete</button>
               </form> 
             </div>
         </Card>
@@ -29,3 +29,20 @@ function get_one (data) {
   }
 
 module.exports = get_one
+
+/*
+.delete-button {
+  width:82%;
+  background-color:red;
+  color:white;
+  margin-left: 1em;
+}
+
+.edit-button {
+  width:20%;
+  background-color:rgb(62, 100, 202);
+  color:white;
+  margin-left: 1em;
+
+}
+*/
