a9e54f00ff39083ad0f4a4b527b7984480f65804
Michael Champlin <mchamplin@sbcglobal.net>
2022-06-05 16:20:22 -0700

    Dropped the years attribute.  Added price-range (& in views)

--- views/sites-get-newform.jsx
+++ views/sites-get-newform.jsx
@@ -5,7 +5,7 @@ function get_new_site_form () {
     console.log("get_new_site_form.jsx")
     return (
       <Default>
-          <main>
+          <main style={{"color":"white"}}>
             <h1>Add A New Site To Review</h1>
             <form method="POST" action="/site">
                 <div className="form_container">
@@ -28,8 +28,8 @@ function get_new_site_form () {
                             <input className="form-control" id="state" name="state" />
                         </div>
                         <div className="form-group col-sm-4">
-                            <label htmlFor="years">Years Of Service</label>
-                            <input type="number" className="form-control" id="years" name="years"/>
+                            <label htmlFor="price_range">Price Range</label>
+                            <input className="form-control" id="price_range" name="price_range"/>
                         </div>
                     </div>
                     <br></br>
